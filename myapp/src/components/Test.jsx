3a57f8693eebb9d98c1e84829d204b5e2ca796fd
Prathibha123Boyapally <prathibhaboyapally123@gmail.com>
2025-01-02 23:24:02 +0530

    reacjs added

---
+++ myapp/src/components/Test.jsx
@@ -0,0 +1,8 @@
+function Test(){
+    return(
+        <div>
+            <h3>This is App component</h3>
+        </div>
+    )
+}
+export default Test;
---
+++ myapp/src/components/proptest.jsx
@@ -0,0 +1,8 @@
+function proptest(props){
+    return(
+        <div>
+            <h2>Hi My name is {props.empname} and my Salary is{props.empsal}</h2>
+        </div>
+    )
+}
+export default proptest;
